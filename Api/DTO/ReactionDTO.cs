1138880902d94aea505aed10d60b02d5d408f837
Kim Konstantin <konakim51@gmail.com>
2022-06-02 14:13:32 +0500

    Added nullable to ReactionDTO

--- Api/DTO/ReactionDTO.cs
+++ Api/DTO/ReactionDTO.cs
@@ -6,13 +6,13 @@ namespace Mattermost.Bot.Api.DTO
     public class ReactionDTO
     {
         [JsonProperty(PropertyName = "post_id")]
-        public string PostId { get; set; }
+        public string? PostId { get; set; }
 
         [JsonProperty(PropertyName = "user_id")]
-        public string UserId { get; set; }
+        public string? UserId { get; set; }
 
         [JsonProperty(PropertyName = "emoji_name")]
-        public string EmojiName { get; set; } 
+        public string? EmojiName { get; set; } 
 
         [JsonProperty(PropertyName = "create_at")]
         public long CreateAt { get; set; }
